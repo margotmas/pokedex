3f01d4c73d5a271f3c32eb13eba0a22e9133d7bb
Margot Masset <margot.voydis@gmail.com>
2023-12-12 16:42:50 +0100

    challenge props

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,21 @@ import PokemonCard from './components/PokemonCard';
   function App() {
     return (
       <div>
-        <PokemonCard />
+        <PokemonCard pokemon={pokemonList[0]}/>
       </div>
     );
   }
+
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+  
   
   export default App;
